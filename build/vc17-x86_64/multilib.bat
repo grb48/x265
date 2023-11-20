e0daded5792a3e0e447311c649c9144b79a8a2a5
grb48 <140832229+grb48@users.noreply.github.com>
2023-11-20 00:25:34 +0000

    Add install to multilib

--- build/vc17-x86_64/multilib.bat
+++ build/vc17-x86_64/multilib.bat
@@ -13,14 +13,14 @@ call "%VS170COMNTOOLS%\..\..\VC\vcvarsall.bat"
 @cd 12bit
 cmake -G "Visual Studio 17 2022" ../../../source -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DMAIN12=ON
 if exist x265.sln (
-  MSBuild /property:Configuration="Release" x265.sln
+  MSBuild /property:Configuration="Release" -m x265.sln
   copy/y Release\x265-static.lib ..\8bit\x265-static-main12.lib
 )
 
 @cd ..\10bit
 cmake -G "Visual Studio 17 2022" ../../../source -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF
 if exist x265.sln (
-  MSBuild /property:Configuration="Release" x265.sln
+  MSBuild /property:Configuration="Release" -m x265.sln
   copy/y Release\x265-static.lib ..\8bit\x265-static-main10.lib
 )
 
@@ -33,12 +33,13 @@ if not exist x265-static-main12.lib (
   msg "%username%" "12bit build failed"
   exit 1
 )
-cmake -G "Visual Studio 17 2022" ../../../source -DEXTRA_LIB="x265-static-main10.lib;x265-static-main12.lib" -DLINKED_10BIT=ON -DLINKED_12BIT=ON
+cmake -G "Visual Studio 17 2022" ../../../source -DEXTRA_LIB="x265-static-main10.lib;x265-static-main12.lib" -DLINKED_10BIT=ON -DLINKED_12BIT=ON -DCMAKE_INSTALL_PREFIX="c:/source/deps/x265/"
 if exist x265.sln (
-  MSBuild /property:Configuration="Release" x265.sln
+  MSBuild /property:Configuration="Release" -m x265.sln
   :: combine static libraries (ignore warnings caused by winxp.cpp hacks)
   move Release\x265-static.lib x265-static-main.lib
   LIB.EXE /ignore:4006 /ignore:4221 /OUT:Release\x265-static.lib x265-static-main.lib x265-static-main10.lib x265-static-main12.lib
+  MSBuild.exe /property:Configuration="Release" INSTALL.vcxproj
 )
 
 pause
